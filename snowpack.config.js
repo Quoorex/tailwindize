ea31fd528db7d177d499c07204717fbb8ce12705
Quoorex <46283604+Quoorex@users.noreply.github.com>
2021-02-19 09:29:25 +0100

    Use snowpack-optimize

--- snowpack.config.js
+++ snowpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   buildOptions: {
     baseUrl: "",
   },
-  plugins: ["@snowpack/plugin-postcss"],
+  plugins: ["@snowpack/plugin-postcss", "@snowpack/plugin-optimize"],
   presets: [
     [
       "@babel/preset-env",
